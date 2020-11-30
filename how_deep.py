6957d95016f7c569df5dab16fd8670a26aeb1d0a
Adrian Larsson <developmentgitemail@protonmail.com>
2020-11-30 20:38:02 +0100

    How deep

--- how_deep.py
+++ how_deep.py
@@ -1,20 +1,9 @@
 def how_deep(structure):
-
-    structure_as_string = ''.join(str(structure))
-
-    list_of_openings = []
-    list_of_close = []
-    for char in structure_as_string:
-        if char == "(" or char == "[":
-            list_of_openings.append(char)
-        if char == ")" or char == "]":
-            list_of_close.append(char)
-
-
-    print(list_of_openings)
-    print(list_of_close)
+    if isinstance(structure, list):
+        return 1 + max(how_deep(item) for item in structure)
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-
-    print(how_deep([1,[2],[3]]))
+    print(how_deep([1,2,3]))
