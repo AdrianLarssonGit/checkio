7b8448ecf2bfedfe6607bab0e7648a3717371b75
Adrian Larsson <developmentgitemail@protonmail.com>
2020-12-05 11:58:55 +0100

    Got a basic version of this working. Alpha 0.0.0.1 ;)

---
+++ reverse_every_ascending.py
@@ -0,0 +1,49 @@
+def reverse_ascending(items):
+    ##Check to see if next item is bigger, add to list.
+    ##If next item is not bigger. Stop and create a new list.
+
+    list_of_lists_to_be_reversed = []
+    start_list = []
+    counter = 0
+    while counter < len(items):
+        i = items[counter]
+        if counter+1 == len(items):
+            next_i_temp = items[-1:]
+            next_i = next_i_temp[0]
+        else:
+            next_i = items[counter+1]
+
+        if next_i == i:
+            start_list.append(i)
+            list_of_lists_to_be_reversed[-1:].append(i)
+        elif next_i > i:
+            start_list.append(i)
+        elif next_i <= i:
+            list_of_lists_to_be_reversed.append(start_list)
+            start_list = []
+            start_list.append(i)
+        else:
+            start_list.append(i)
+            list_of_lists_to_be_reversed.append(start_list)
+
+        counter += 1
+
+    list_of_lists_to_be_reversed.append(start_list)
+    print(list_of_lists_to_be_reversed)
+    for list in list_of_lists_to_be_reversed:
+        list.reverse()
+
+    final_list = []
+    for list in list_of_lists_to_be_reversed:
+        for item in list:
+            final_list.append(item)
+
+    print(final_list)
+
+
+    # your code here
+    return None
+
+
+if __name__ == '__main__':
+    print(reverse_ascending([1, 2, 3, 4, 5]))
